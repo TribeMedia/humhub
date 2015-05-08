c58e07497829f163efbc38b5d17c4eb0143e59e3
GQAdonis <gqadonis@me.com>
2015-05-08 10:20:41 -0500

    add products module

--- themes/TribeMedia/views/layouts/head.php
+++ themes/TribeMedia/views/layouts/head.php
@@ -2,10 +2,8 @@
 
 <!-- start: CSS -->
 <link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/datepicker.css?ver=<?php echo $ver; ?>" rel="stylesheet">
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/flatelements.css?ver=<?php echo $ver; ?>" rel="stylesheet">
 <link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/theme.css?ver=<?php echo $ver; ?>" rel="stylesheet">
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/flatelements.css?ver=<?php echo $ver; ?>" rel="stylesheet">
 <link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/variables.css" rel="stylesheet">
-<link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/bootswatch.min.css" rel="stylesheet">
 <!-- end: CSS -->
 
