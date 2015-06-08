b75e91f2ec808a332fa8e4fead524f1e3e50064c
Travis James <gqadonis@me.com>
2015-06-08 18:35:41 +0000

    create Prime theme

---
+++ themes/Prime/views/layouts/head.php
@@ -0,0 +1,8 @@
+<?php
+
+Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . '/css/datepicker.css');
+Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . '/css/theme.css');
+Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . '/css/flatelements.css');
+Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . '/font/open_sans/open-sans.css');
+?>
+
